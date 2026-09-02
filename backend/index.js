111f754a0fe2c4609b1d46de0c4581638a5bb145
ABHINAV KAUSHAL <abhinavkaushaldos5988@gmail.com>
2026-09-02 22:26:42 +0530

    update code

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDb from "./config/db.js"
 dotenv.config()
 const app=express()
 const port=process.env.PORT || 5000
 
 app.listen(port,()=>{
-  console.log('server started at ${port}')
+  connectDb()
+  console.log(`server started at ${port}`)
 })
